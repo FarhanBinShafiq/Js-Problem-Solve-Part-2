08e4321d1bd29b0c2c72dc1c828021369afe5149
FarhanBinShafiq <farhan.shafiq@northsouth.edu>
2022-10-16 23:41:15 +0600

    grading Policy

--- grading-system-switch/script.js
+++ grading-system-switch/script.js
@@ -1,30 +1,40 @@
 
 let gradeSubmitButton = document.getElementById('grade-submit-button');
 
-gradeSubmitButton.addEventListener('click', (e) => {
+gradeSubmitButton.addEventListener('click', () => {
     let gradingInput = document.getElementById('grading-input').value;
     console.log(gradingInput);
 
     let gradeShow = document.getElementById('grade-show');
+    let p = document.createElement('p');
 
     switch (true) {
         case gradingInput >= 90:
-            let grade = document.createElement('p');
+            p.innerText = `Your Grade is A`;
+            gradeShow.appendChild(p);
             console.log("Your Grade is A");
             break;
         case gradingInput >= 80:
+            p.innerText = `Your Grade is B`;
+            gradeShow.appendChild(p);
             console.log("Your Grade is B");
             break;
         case gradingInput >= 70:
+            p.innerText = `Your Grade is C`;
+            gradeShow.appendChild(p);
             console.log("Your Grade is C");
             break;
         case gradingInput >= 60:
+            p.innerText = `Your Grade is D`;
+            gradeShow.appendChild(p);
             console.log("Your Grade is D");
             break;
         default:
+            p.innerText = `Your Grade is F`;
+            gradeShow.appendChild(p);
             console.log("Your Garde is F")
     }
 
-
-
 })
+
+
